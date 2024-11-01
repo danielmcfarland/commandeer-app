a22ef59a5c9a5ec0582322e1ab161f40ccdbe8b3
Daniel McFarland <daniel.mcfarland@me.com>
2024-11-01 22:28:23 +0000

    Configuring spatie/laravel-http-logger

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\HttpLogger\Middlewares\HttpLogger;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(HttpLogger::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
