ba8273e6310fb6ef89ec3da472483719d9a04f00
Daniel McFarland <daniel.mcfarland@me.com>
2024-11-04 18:02:28 +0000

    Running DispatchCheckInJobs Every 15 Minutes

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
 use App\Console\Commands\DispatchCheckInJobs;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-Schedule::command(DispatchCheckInJobs::class)->hourly();
+Schedule::command(DispatchCheckInJobs::class)->everyFifteenMinutes();
