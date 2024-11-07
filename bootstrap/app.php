9c0f0372bd72bb4085555a460c06d9965b741681
Daniel McFarland <daniel.mcfarland@me.com>
2024-11-07 14:11:15 +0000

    Adding Sentry to handle unhandled errors.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
+use Sentry\Laravel\Integration;
 use Spatie\HttpLogger\Middlewares\HttpLogger;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -20,5 +21,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(HttpLogger::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->create();
