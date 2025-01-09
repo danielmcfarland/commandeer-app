72db46971d5f859d71b5742eeb27fc564d8cd913
Daniel McFarland <daniel.mcfarland@me.com>
2025-01-09 20:10:11 +0000

    Adding endpoint for nanomdm callback

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::domain(config('app.domain'))->post('mdm', function (Request $request) {
+    //
+})->name('mdm-callback')->withoutMiddleware([
+    ValidateCsrfToken::class,
+]);
