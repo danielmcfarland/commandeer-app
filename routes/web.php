2693f267d5b9318415f931329e387804ab20c427
Daniel McFarland <daniel.mcfarland@me.com>
2025-04-28 20:42:42 +0100

    Actually implementing routing for mdm enrollment dep.

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,19 @@ use App\Http\Controllers\MdmCallbackController;
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::domain(config('app.domain'))
-    ->get('MDMServiceConfig', function () {
+Route::domain('{account}.' . config('app.domain'))
+    ->get('MDMServiceConfig', function (string $account) {
         return response()->json([
-            'dep_anchor_certs_url' => route('mdm.dep_anchor_certs_url'),
-            'dep_enrollment_url' => route('mdm.callback'),
+            'dep_anchor_certs_url' => route('mdm.dep_anchor_certs_url', [
+                'account' => $account,
+            ]),
+            'dep_enrollment_url' => route('mdm.callback', [
+                'account' => $account,
+            ]),
         ]);
     });
 
-Route::domain(config('app.domain'))
+Route::domain('{account}.' . config('app.domain'))
     ->prefix('mdm')
     ->name('mdm')
     ->group(function () {
