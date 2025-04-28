d42a99aee3365ae7f6ceff200e6c6cc1bc44f944
Daniel McFarland <daniel.mcfarland@me.com>
2025-04-28 20:22:52 +0100

    Updating web routes to allow for additional mdm functionality.

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,25 @@ use App\Http\Controllers\MdmCallbackController;
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::domain(config('app.domain'))->post('mdm', MdmCallbackController::class)
-    ->name('mdm-callback')
-    ->withoutMiddleware([
-        ValidateCsrfToken::class,
-    ]);
+Route::domain(config('app.domain'))
+    ->get('MDMServiceConfig', function () {
+        return response()->json([
+            'dep_anchor_certs_url' => route('mdm.dep_anchor_certs_url'),
+            'dep_enrollment_url' => route('mdm.callback'),
+        ]);
+    });
+
+Route::domain(config('app.domain'))
+    ->prefix('mdm')
+    ->name('mdm')
+    ->group(function () {
+        Route::post('/', MdmCallbackController::class)
+            ->name('.callback')
+            ->withoutMiddleware([
+                ValidateCsrfToken::class,
+            ]);
+
+        Route::get('/dep_anchor_certs_url', function () {
+            return response()->json();
+        })->name('.dep_anchor_certs_url');
+    });
